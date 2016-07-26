899747bb9c62a5576371e4b36ecdd07657ab4acd
ckshei <cks856@gmail.com>
2016-07-26 15:49:45 -0400

    added user.rb

--- lib/restaurants.rb
+++ lib/restaurants.rb
@@ -5,8 +5,7 @@ require 'open-uri'
 
 class Restaurant
 
-  attr_reader :link, :doc
-  attr_accessor :cuisine, :description, :res_times
+  attr_accessor :link, :doc, :cuisine, :description, :res_times
 
   def initialize(link)
     @link = link
@@ -22,17 +21,3 @@ class Restaurant
 end
 
 binding.pry
-
-# # module Tastable
-# doc = Nokogiri::HTML(open("https://m.opentable.com/restaurants/scalini-fedeli/6417?PartySize=6"))
-# cuisine = doc.css('div#ft').css('.dot-before').text
-# description = doc.css('div#DescriptionText').text
-# reservation_times = []
-# doc.css('div.col-xs-12 div#restaurantAvailabilityResults div#ulSlots a span').each do |x|
-#   reservation_times << x.text
-# end
-# binding.pry
-
-# # end
-# object = Tastable::Restaurant_hash.new
-# binding.pry
--- lib/user.rb
+++ lib/user.rb
@@ -4,22 +4,28 @@ require 'json'
 require 'pry'
 
 module Tastable
-  class Restaurant_hash
+  class User
 
-    def get(zipcode)
+    attr_accessor :zipcode, :party_size, :nearby_restaurants
+
+    def initialize(zipcode, party_size)
+      @zipcode = zipcode
+      @party_size = party_size
+    end
+
+    def get_nearby_restaurants
       conn = Faraday.new(:url => 'https://opentable.herokuapp.com') do |faraday|
         faraday.request  :url_encoded             # form-encode POST params
         faraday.response :logger                  # log requests to STDOUT
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
-      faraday_object = conn.get '/api/restaurants', {:zip => zipcode }
+      faraday_object = conn.get '/api/restaurants', {:zip => self.zipcode }
       # binding.pry
       hash = JSON.parse(faraday_object.body)
-      restaurants = hash["restaurants"]
-      binding.pry
+      @nearby_restaurants = hash["restaurants"]
     end
   end
 
 end
-object = Tastable::Restaurant_hash.new
+user1 = Tastable::User.new(11211,3)
 binding.pry
