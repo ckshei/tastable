7cd25bec7cb2fd1cea7f13d634306c17018899b7
ckshei <cks856@gmail.com>
2016-07-26 09:12:30 -0400

    Created a hash of restaurants from the zip code

--- lib/tastable.rb
+++ lib/tastable.rb
@@ -1,5 +1,24 @@
-require "tastable/version"
+# require "tastable/version"
+require 'faraday'
+require 'json'
+require 'pry'
 
 module Tastable
-  # Your code goes here...
+  class Restaurant_hash
+
+    def get(zipcode)
+      conn = Faraday.new(:url => 'https://opentable.herokuapp.com') do |faraday|
+        faraday.request  :url_encoded             # form-encode POST params
+        faraday.response :logger                  # log requests to STDOUT
+        faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+      faraday_object = conn.get '/api/restaurants', {:zip => zipcode }
+      # binding.pry
+      hash = JSON.parse(faraday_object.body)
+      binding.pry
+    end
+  end
+
 end
+object = Tastable::Restaurant_hash.new
+binding.pry
